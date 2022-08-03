c103b8de230ae1265e46635da4e8c420218146de
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 01:01:44 -0700

    checking the commit setting

--- src/NumberOfProvinces.java
+++ src/NumberOfProvinces.java
@@ -7,7 +7,7 @@ public class NumberOfProvinces {
     static void dfs(int current, ArrayList<ArrayList<Integer>> adj, int v, int[] visited){
         //adj matrix will have 1 if they are connected,
         //so the condition is if they are connected and not visited then mark them visited
-        //and apply dfs to next elements
+        //and apply dfs to next elements onwards
         for(int i=0; i<v; i++){
             if(adj.get(current).get(i) ==1 && visited[i]==0){
                 visited[i]= 1;
