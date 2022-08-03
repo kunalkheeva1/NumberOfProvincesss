ccd1ee57a2194acf3c71b5a4166fbe0c02741aa2
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 01:00:30 -0700

    added the comments for the better understanding

--- src/NumberOfProvinces.java
+++ src/NumberOfProvinces.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 public class NumberOfProvinces {
 
+
+    //dfs method
     static void dfs(int current, ArrayList<ArrayList<Integer>> adj, int v, int[] visited){
+        //adj matrix will have 1 if they are connected,
+        //so the condition is if they are connected and not visited then mark them visited
+        //and apply dfs to next elements there
         for(int i=0; i<v; i++){
             if(adj.get(current).get(i) ==1 && visited[i]==0){
                 visited[i]= 1;
@@ -12,11 +17,15 @@ public class NumberOfProvinces {
     }
 
     static int numberOfProvinces(ArrayList<ArrayList<Integer>> adj, int v){
+
+        //first of all get a visited array to keep the record for the visited elements
+        // and mark all the elements not visited in it initially
         int[] visited = new int[v];
         for(int i =0; i<v; i++){
             visited[i] =1;
         }
         int count =0;
+        //increase the count after every other depth first search
         for(int i=0; i<v; i++){
             if(visited[i] ==0){
                 visited[i] =1;
